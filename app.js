c65e40ca505b81e773a245995840a2098c4080da
allannunes06 <85025033+allannunes06@users.noreply.github.com>
2021-12-28 19:35:23 -0300

    My Pokedex

---
+++ app.js
@@ -0,0 +1,36 @@
+
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+const generatePokemonPromises = () => Array(898).fill().map((_, index) =>
+    fetch(getPokemonUrl(index + 1)).then(response => response.json()))
+    
+const generateHTML = pokemons => {
+
+    return pokemons.reduce((accumulator, pokemon) => {
+        const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+        accumulator += `
+    <li class="card ${types[0]}">
+    <img class="card-image" alt="${pokemon.name}"src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemon.id}.png"/>
+    <h2 class="card-title">#${pokemon.id}. ${pokemon.name}</h2>
+    <p class="card-subtitle">${types.join(' | ')}</p>
+    </li>`
+
+        return accumulator
+    }, '')
+}
+
+const insertPokemonIntoPage = pokemons => {
+    const ul = document.querySelector('[data-js="pokedex"]')
+    ul.innerHTML = pokemons
+}
+
+const fetchPokemon = () => {
+
+    const pokemonPromises = generatePokemonPromises()
+
+    Promise.all(pokemonPromises)
+        .then(generateHTML)
+        .then(insertPokemonIntoPage)
+}
+
+fetchPokemon()
